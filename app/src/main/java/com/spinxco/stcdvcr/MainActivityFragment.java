6c1959fdb1d2c370037048c7abb2e70da6a03e79
Nathaniel Clayton <nxc@spinx.spinxco.com>
2015-10-30 10:56:32 -0400

    Initial commit

---
+++ app/src/main/java/com/spinxco/stcdvcr/MainActivityFragment.java
@@ -0,0 +1,51 @@
+package com.spinxco.stcdvcr;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
+
+    public MainActivityFragment() {
+    }
+
+    View view;
+    Button inspectButton;
+    Button submitButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_main, container, false);
+        inspectButton = (Button) view.findViewById(R.id.inspectButton);
+        inspectButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.inspectButton:
+                inspectionReport();
+                break;
+        }
+    }
+
+    public void inspectionReport() {
+
+        InspectionFragment frag = new InspectionFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.main_fragment, frag);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+}
